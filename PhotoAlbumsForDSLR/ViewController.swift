2134b30eec124df91092dd86466634846acabd5c
tochiba <c104kn@gmail.com>
2017-09-04 00:18:32 +0900

    Initial Commit

---
+++ PhotoAlbumsForDSLR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoAlbumsForDSLR
+//
+//  Created by Toshiki Chiba on 2017/09/04.
+//  Copyright © 2017年 Toshiki Chiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
